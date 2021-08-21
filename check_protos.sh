ebfecbb490d7a6ad59596fc84032715652e4d3f8
Lindsey Gray <lindsey.gray@gmail.com>
2021-08-21 15:48:04 -0500

    some first things to take care of

---
+++ check_protos.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+GOOL_ROOT=$(pwd)
+pushd ../
+mkdir -p go
+export GOPATH=$(pwd)/go
+PATH="$PATH:$(go env GOPATH)/bin"
+
+go get google.golang.org/protobuf/cmd/protoc-gen-go
+go get google.golang.org/grpc/cmd/protoc-gen-go-grpc
+
+git clone https://github.com/blockcollider/bcjs.git
+cd bcjs/protos
+for pb in $(ls *.proto)
+do
+    sed -i "s/package bcsdk\;/option go_package = \"github.com\/overline\-mining\/gool\/src\/protos\"\;/g" ${pb}
+done
+protoc \
+  --go_out=${GOOL_ROOT}/src/protos --go_opt=paths=source_relative \
+  --go-grpc_out=${GOOL_ROOT}/src/protos --go-grpc_opt=paths=source_relative \
+  *.proto
+
+popd
