bbc57577dfb872c133c6830a1c6eacf1e104b08c
Lindsey Gray <lindsey.gray@gmail.com>
2021-12-23 05:50:19 -0600

    working on getting peer list and connecting to swarm

--- go.mod
+++ go.mod
@@ -4,10 +4,10 @@ go 1.16
 
 require (
 	github.com/alecthomas/kong v0.2.0
-	github.com/ethereum/go-ethereum v1.9.15 // indirect
+	github.com/golang/protobuf v1.4.2
 	github.com/pkg/errors v0.9.1
 	github.com/wavesplatform/gowaves v0.9.1-0.20210819143633-86b30fcdc348
+	github.com/xgfone/bt v0.4.2-0.20210905131943-4314dbe52089
 	go.uber.org/zap v1.10.0
 	google.golang.org/grpc v1.32.0
-	google.golang.org/protobuf v1.25.0
 )
--- src/node/node.go
+++ src/node/node.go
@@ -11,9 +11,19 @@ import (
   
   "github.com/overline-mining/gool/src/common"
 
-  "go.uber.org/zap"
-)
+  "fmt"
 
+  "go.uber.org/zap"
+
+	"github.com/xgfone/bt/metainfo"
+	"github.com/xgfone/bt/tracker"
+  "github.com/xgfone/bt/dht"
+  "net"
+	"strconv"
+	"sync"
+  "strings"
+)
+  
 var (
   logLevel = flag.String("log-level", "INFO", "Logging level. Supported levels: DEBUG, INFO, WARN, ERROR, FATAL. Default logging level INFO.")
   blockchainType = flag.String("blockchain-type", "mainnet", "Blockchain type: mainnet/testnet/integration")
@@ -21,6 +31,89 @@ var (
   dbFileDescriptors = flag.Int("db-file-descriptors", 500, "Maximum allowed file descriptors count that will be used by state database. Default value is 500.")
 )
 
+func getPeersFromTrackers(id, infohash metainfo.Hash, trackers []string) (peers []string) {
+	c, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+  
+	resp := tracker.GetPeers(c, id, infohash, trackers)
+	for _, r := range resp {
+		for _, addr := range r.Resp.Addresses {
+			addrs := addr.String()
+			nonexist := true
+			for _, peer := range peers {
+				if peer == addrs {
+					nonexist = false
+					break
+				}
+			}
+
+			if nonexist {
+				peers = append(peers, addrs)
+			}
+		}
+	}
+
+	return
+}
+
+type testPeerManager struct {
+	lock  sync.RWMutex
+	peers map[metainfo.Hash][]metainfo.Address
+}
+
+func newTestPeerManager() *testPeerManager {
+	return &testPeerManager{peers: make(map[metainfo.Hash][]metainfo.Address)}
+}
+
+func (pm *testPeerManager) AddPeer(infohash metainfo.Hash, addr metainfo.Address) {
+	pm.lock.Lock()
+	var exist bool
+	for _, orig := range pm.peers[infohash] {
+		if orig.Equal(addr) {
+			exist = true
+			break
+		}
+	}
+	if !exist {
+		pm.peers[infohash] = append(pm.peers[infohash], addr)
+	}
+	pm.lock.Unlock()
+}
+
+func (pm *testPeerManager) GetPeers(infohash metainfo.Hash, maxnum int,
+	ipv6 bool) (addrs []metainfo.Address) {
+	// We only supports IPv4, so ignore the ipv6 argument.
+	pm.lock.RLock()
+	_addrs := pm.peers[infohash]
+	if _len := len(_addrs); _len > 0 {
+		if _len > maxnum {
+			_len = maxnum
+		}
+		addrs = _addrs[:_len]
+	}
+	pm.lock.RUnlock()
+	return
+}
+
+func onSearch(infohash string, ip net.IP, port uint16) {
+	addr := net.JoinHostPort(ip.String(), strconv.FormatUint(uint64(port), 10))
+	fmt.Printf("%s is searching %s\n", addr, infohash)
+}
+
+func onTorrent(infohash string, ip net.IP, port uint16) {
+	addr := net.JoinHostPort(ip.String(), strconv.FormatUint(uint64(port), 10))
+	fmt.Printf("%s has downloaded %s\n", addr, infohash)
+}
+
+func newDHTServer(id metainfo.Hash, addr string, pm dht.PeerManager) (s *dht.Server, err error) {
+	conn, err := net.ListenPacket("udp", addr)
+	if err == nil {
+		c := dht.Config{ID: id, PeerManager: pm, OnSearch: onSearch, OnTorrent: onTorrent}
+		s = dht.NewServer(conn, c)
+	}
+	return
+}
+    
 func main() {
   flag.Parse()
 
@@ -48,6 +141,95 @@ func main() {
 	}
 
   zap.S().Info(ntpTime)
+
+  id := metainfo.NewRandomHash()
+  // bcnode mainnet infohash
+  infoHash := metainfo.NewHashFromHexString("716ca5c568509b3652a21dd076e1bf842583267c")
+          
+  trackers := []string{
+    "udp://tds-r3.blockcollider.org:16060/announce",
+  	"udp://104.207.130.112:16060/announce",
+    "udp://139.180.221.213:16060/announce",
+    "udp://52.208.110.140:16060/announce",
+    "udp://internal.xeroxparc.org:16060/announce",
+    "udp://reboot.alcor1436.com:16060/announce",
+    "udp://sa1.alcor1436.com:16060/announce",
+  }
+
+  peers := getPeersFromTrackers(id, infoHash, trackers)
+	if len(peers) == 0 {
+		fmt.Println("no peers")
+		return
+	}
+
+  zap.S().Info(peers)
+
+  pm := newTestPeerManager()
+  server, err := newDHTServer(id, ":16061", pm)
+  if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer server.Close()
+
+  go server.Run()
+
+  //wait for server to start
+  time.Sleep(time.Second)
+
+  peersnoport := []string{}
+  for _, peer := range peers {
+    peersnoport = append(peersnoport, strings.Split(peer, ":")[0])
+  }
+
+  server.Bootstrap([]string{
+    "router.bittorrent.com:6881",
+    "router.utorrent.com:6881",
+    "dht.transmissionbt.com:6881",
+    "router.bitcomet.com:6881",
+    "dht.aelitis.com:6881",
+    "18.210.15.44:16061",
+  })
+
+  zap.S().Info("Waiting for bootstrap to complete...")
+  time.Sleep(time.Second * 30)
+  zap.S().Info("Done waiting...")
+
+  
+  for _, peer := range peersnoport {
+    pm.AddPeer(infoHash, metainfo.NewAddress(net.ParseIP(peer), 16061))
+  }
+    
+  zap.S().Info(peers)
+
+  // wait for DHT table to build from bootstrap
+  time.Sleep(time.Second * 5)
+  
+  zap.S().Infof("server: %v", server.Node4Num())
+
+  server.GetPeers(infoHash, func(r dht.Result) {
+    if len(r.Peers) == 0 {
+      fmt.Printf("no peers for %s\n", infoHash)
+    } else {
+      for _, peer := range r.Peers {
+        fmt.Printf("%s: %s\n", infoHash, peer.String())
+      }
+    }
+  })
+
+  time.Sleep(time.Second * 2)
+
+  server.GetPeers(infoHash, func(r dht.Result) {
+    if len(r.Peers) == 0 {
+      fmt.Printf("no peers for %s\n", infoHash)
+    } else {
+      for _, peer := range r.Peers {
+        fmt.Printf("%s: %s\n", infoHash, peer.String())
+      }
+    }
+  })
+
+  time.Sleep(time.Second * 60)
 }
 
 func getNtp(ctx context.Context) (types.Time, error) {
