9124877402b299ad403414e887d25cd592ab3f45
Lindsey Gray <lindsey.gray@gmail.com>
2022-02-10 18:19:35 -0600

    [no ci] update protos-check script

--- check_protos.sh
+++ check_protos.sh
@@ -10,14 +10,14 @@ go get google.golang.org/protobuf/cmd/protoc-gen-go
 go get google.golang.org/grpc/cmd/protoc-gen-go-grpc
 
 git clone https://github.com/blockcollider/overline-proto.git
-cd overline-proto
-for pb in $(ls protos/*.proto)
+for pb in $(ls *.proto)
 do
     sed -i.old '2s;^;\noption\ go_package\ \=\ \"github.com\/gool\/src\/protos\"\;\n;' ${pb}
 done
 protoc \
   --go_out=${GOOL_ROOT}/src/protos --go_opt=paths=source_relative \
   --go-grpc_out=${GOOL_ROOT}/src/protos --go-grpc_opt=paths=source_relative \
-  protos/*.proto
+  *.proto
 
+popd
 popd
