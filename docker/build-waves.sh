804b245cf14d31806cb1ea54568cf340381c3ae3
Lindsey Gray <lindsey.gray@gmail.com>
2022-03-31 11:18:43 -0500

    get waves building

--- docker/build-waves.sh
+++ docker/build-waves.sh
@@ -13,7 +13,7 @@ git clone https://github.com/wavesplatform/Waves.git -b ${WAVES_VERSION}
 pushd Waves
 
 sed -i 's/buster/unstable/' docker/Dockerfile
-{ ./build-with-docker.sh && docker buildx build --platform ${ARCH} --tag ${REPO_NAME}/waves:${GOOL_VERSION} docker ${DO_PUSH} } || { echo "WAVES BUILD FAILED"; popd; popd; rm -rf $(pwd)/waves-build; exit 1; }
+./build-with-docker.sh && docker buildx build --platform ${ARCH} --tag ${REPO_NAME}/waves:${GOOL_VERSION} docker ${DO_PUSH} || { echo "WAVES BUILD FAILED"; popd; popd; rm -rf $(pwd)/waves-build; exit 1; }
 
 popd
 
